1631b412b03557735d419964fe4883a661f0fb81
Qasim Shabbir <qasimshabbir@gmail.com>
2018-08-29 08:51:20 +0500

    Added test files

---
+++ 02-ICO/test/e_r_c20_interface.js
@@ -0,0 +1,8 @@
+contract('ERC20Interface', function(accounts) {
+  it("should assert true", function(done) {
+    var token;
+    return token. = ERC20Interface.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
---
+++ 02-ICO/test/q_sols_token.js
@@ -0,0 +1,7 @@
+contract('QSolsToken', function(accounts) {
+  it("should assert true", function(done) {
+    var q_sols_token = QSolsToken.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
